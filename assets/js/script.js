94f7bf51b4f53a430c40ec0306359981eb9e2e13
EulerRail <aherta@163.com>
2025-08-12 22:25:02 +0800

    加入json配置功能，字体

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
-fetch('data.json')
-    .then(response => response.json())
+fetch('data.json') /** * 从data.json文件获取数据并处理 * 使用fetch API异步获取JSON数据 * 然后处理数据并更新页面样式和内容 */
+    .then(response => response.json()) //  将响应解析为JSON格式
     .then(data => {
-        var bg_img = document.getElementById('bg-img');
-        src = "../img/bg/"+data.data.bg.src
-        bg_img.style.setProperty('--src',`url(${src})`);
+        init(data.data,data); //  初始化页面样式和内容
     })
-    .catch(error => {
+    .catch(error => { //  捕获并处理可能的错误
         console.error('Error fetching JSON:', error);
     });
+
+function setStyle(key, value) {
+    document.documentElement.style.setProperty(`--${key}`, value);
+}
+function setStyleFile(key, src) {
+    document.documentElement.style.setProperty(`--${key}`, `url(${"../" + src})`);
+    // console.log(`Set style for ${key}: url(${"../" + src})`);
+}
+function changeContent(key, value) {
+    document.getElementById(key).textContent = value;
+}
+function init(data ,all){
+    setStyleFile('bg', data.bg.src) //  设置背景图片样式
+        setStyleFile('icon', data.icon.src) //  设置图标样式 
+        color_data = data.color_list //  获取颜色数据
+        Object.entries(color_data).forEach(([k, v]) => //  遍历颜色数据并设置样式
+            setStyle(k, v)
+        );
+        changeContent('nav-name', data.name); //  设置导航名称文本内容
+}
